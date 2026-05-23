68f67b6f1159d40bf293e46919e581bacb2a9ae2
danielly rodrigues moura barreto <danybarreto2008@gmail.com>
2026-05-23 20:11:40 -0300

    segunda pagina

--- app/views.py
+++ app/views.py
@@ -10,8 +10,38 @@ def inicio (request):
     }
     return render (request, 'app/inicio.html', {'serie': dados_serie})
 
-def equipe(request):
-    return render(request, 'app/equipe.html')
+def equipe (request):
+    lista_personagens = [
+        {
+            'nome': 'Alex',
+            'idade': '18',
+            'posicao': 'Especialista em Esportes',
+            'local': 'Beverly Hills',
+            'imagem': 'app/img/alexelenco.png' 
+        },
+        {
+            'nome': 'Clover',
+            'idade': '18',
+            'posicao': 'Especialista em Combate',
+            'local': 'Beverly Hills',
+            'imagem': 'app/img/cloverelenco.png' 
+        },
+        {
+            'nome': 'Samantha (Sam)',
+            'idade': '18',
+            'posicao': 'Líder / Estrategista',
+            'local': 'Beverly Hills',
+            'imagem': 'app/img/samelenco.png' 
+        },
+        {
+            'nome': 'Jerry Lewis',
+            'idade': '60',
+            'posicao': 'Fundador da WOOHP',
+            'local': 'Londres',
+            'imagem': 'app/img/jerryelenco.png' 
+        },
+    ]
+    return render(request, 'app/equipe.html', {'elenco': lista_personagens})
 
 def sobre(request):
     return render (request, 'app/sobre.html')
