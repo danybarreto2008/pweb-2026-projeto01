ebab7d84a97dbb163d91d2d7e2df774eddd20711
danielly rodrigues moura barreto <danybarreto2008@gmail.com>
2026-05-22 21:29:54 -0300

    pagina_inicio

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+def inicio (request):
+    dados_serie = {
+        'nome': 'Totally Spies!',
+        'estreia': '2001',
+        'genero': 'Ação, Aventura, Comédia e Espionagem',
+        'publico': 'Infantil e Adolescente',
+        'estilo': 'Inspirado em filmes de espionagem como 007.'
+    }
+    return render (request, 'app/inicio.html', {'serie': dados_serie})
+
+def equipe(request):
+    return render(request, 'app/equipe.html')
+
+def sobre(request):
+    return render (request, 'app/sobre.html')
